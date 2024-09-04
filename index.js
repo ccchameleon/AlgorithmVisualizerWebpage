6684592f2c8442fcb082f06aa365433678c13de1
name <alexn5673@gmail.com>
2024-09-03 17:03:51 -0700

    added descriptors and small tweaks to button logic. still needs work

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
-//used to scale multiple functions in program
-let numOfBars = 10; //default number of bars
-let minRange = 1;   //range of random ints to use
-let maxRange = 10;  
+//File driving logic for buttons, functions, etc.
+
 let unsortedArr = new Array(numOfBars);
+//used to scale multiple functions in program
+let numOfBars = 20; 
+
+//range of random vals corresponding to arr elems
+let minRange = 1;   
+let maxRange = 10;  
+
 let barScale = 25; //default bar height scale
-let speedScale = 50; //default sorting speed
+let speedScale = 100; //default sorting speed
+
+//Button flags
 let stopClicked = false;
-// let sortingOption = bubbleSort(unsortedArr); //default sorting option
+let sortBtnClicked = false;
+let newArrBtnClicked = false;
 
+//  vars used to resume sorting at same position as when stopped
+let currI = 0;
+let currJ = 0;
+let currK = 0;
 
-//helper for generating random array
+//                                          ----------------- USER PROMPTS -----------------
+
+function changeNumBars(){
+    numOfBars = prompt("Enter number of bars (2-40)");
+    if(numOfBars > 40){
+        new Notification("Too many bars!")
+        return;
+    } else if (numOfBars < 2){
+        new Notification("Not enough bars!")
+        return;
+    }
+    barContainer.innerHTML="";
+    unsortedArr = new Array(numOfBars);
+    createNewArray();
+    renderBar(unsortedArr);
+}
+
+//                                          ----------------- HELPER FUNCTS -----------------
+
 function randomInt(min,max){
     return Math.floor(Math.random()*(max - min + 1)) + min;
 };
 
-function createRandomArray(){
-    for(let i=0; i<numOfBars; ++i){
-        unsortedArr[i] = randomInt(minRange,maxRange);
-    }
+function createNewArray(){
+        for(let i=0; i<numOfBars; ++i){
+            unsortedArr[i] = randomInt(minRange,maxRange);
+        }
 };
 
 function deleteBars(){
@@ -39,21 +69,7 @@ function renderBar(arr){
     }
 };
 
-function barCount(){
-    numOfBars = prompt("Enter number of bars (2-40)");
-    if(numOfBars > 40){
-        new Notification("Too many bars!")
-        return;
-    } else if (numOfBars < 2){
-        new Notification("Not enough bars!")
-        return;
-    }
-    barContainer.innerHTML="";
-    unsortedArr = new Array(numOfBars);
-    createRandomArray();
-    renderBar(unsortedArr);
-}
-
+//  -----------------SPEED INPUT-----------------
 function changeSpeed(){
     speedScale = prompt("Enter speed (Fastest 0 - 500 Slowest)");
     speedScale = Number(speedScale);
@@ -66,64 +82,79 @@ function changeSpeed(){
 }
 
 
+
+//  -----------------SLEEP FUNCT-----------------
 function sleep(ms){
     return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
+//  -----------------STOP FUNCT-----------------
 function stopRunning(){
     stopClicked = true;
 }
 
-function resume(){
+//  -----------------RESUME FUNCT-----------------
+function resumeRunning(){
     stopClicked = false;
-    bubbleSort(unsortedArr);
+    bubbleSort(unsortedArr, currI, currJ, currK);
 }
 
-async function bubbleSort(arr){
+//  -----------------BUBBLE SORT-----------------
+async function bubbleSort(arr, currI, currJ, currK){
     let bars = document.getElementsByClassName("bar");
-    for(let i =0; i < arr.length; ++i){
-        for(let j = 0; j < arr.length-i-1; ++j){
-            if(arr[j]>arr[j+1]){
-                //
-                for(let k=0; k<bars.length;++k){
-                    if(k != j && k != j+1){
-                        bars[k].style.backgroundColor = "lightskyblue";
+    for(let i = currI; i < arr.length; ++i){
+        for(let j = currJ; j < arr.length-i-1; ++j){
+            if(!newArrBtnClicked){
+                if(!stopClicked){
+                if(arr[j]>arr[j+1]){
+                    for(let k = currK; k < bars.length;++k){
+                        if(k != j && k != j+1){
+                            bars[k].style.backgroundColor = "lightskyblue";
+                        }
                     }
+                    let temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    bars[j].style.height = arr[j] * barScale + "px";
+                    bars[j].style.backgroundColor = "blue";
+                    bars[j+1].style.height = arr[j+1] * barScale + "px";
+                    bars[j+1].style.backgroundColor = "blue";
+                    await sleep(speedScale);
+                    barPosition = j;
                 }
-                let temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-                bars[j].style.height = arr[j] * barScale + "px";
-                bars[j].style.backgroundColor = "blue";
-                bars[j+1].style.height = arr[j+1] * barScale + "px";
-                bars[j+1].style.backgroundColor = "blue";
-                if(stopClicked == true){
-                    return;
-                }
-                await sleep(speedScale);
+                bars[j].style.backgroundColor = "lightskyblue";
+                bars[j+1].style.backgroundColor = "lightskyblue";
             }
-            bars[j].style.backgroundColor = "lightskyblue";
-            bars[j+1].style.backgroundColor = "lightskyblue";
         }
-    await sleep(speedScale);
     }
+    await sleep(speedScale);
+}
+console.log(barPosition);
     return arr;
 };
 
+//  -----------------LOAD DEFAULT ARRAY & BARS-----------------
 document.addEventListener("DOMContentLoaded", function(){
-    createRandomArray();
+    createNewArray();
     renderBar(unsortedArr);
+    console.log(sortBtnClicked)
 });
 
+//                                          ----------------- BUTTONS -----------------
+
+//  -----------------NEW ARRAY-----------------
 document.addEventListener("DOMContentLoaded", function(){
-    const randomize_btn = document.getElementById("randomizeBtn");
-    randomize_btn.addEventListener("click", function(){
-        barContainer.innerHTML="";
-        createRandomArray();
-        renderBar(unsortedArr);
-    })
+    const newArray_btn = document.getElementById("randomizeBtn");
+        newArray_btn.addEventListener("click", function(){
+            barContainer.innerHTML="";
+            createNewArray();
+            renderBar(unsortedArr);
+            newArrBtnClicked = true;
+        })
 });
 
+
+//  -----------------STOP-----------------
 document.addEventListener("DOMContentLoaded", function(){
     const stop_btn = document.getElementById("stopBtn");
     stop_btn.addEventListener("click", function(){
@@ -131,13 +162,16 @@ document.addEventListener("DOMContentLoaded", function(){
     })
 })
 
+//  -----------------SORT-----------------
 document.addEventListener("DOMContentLoaded", function(){
     const sort_btn = document.getElementById("sortBtn");
     sort_btn.addEventListener("click", function(){
-        renderBar(bubbleSort(unsortedArr));
+        sortBtnClicked = true;
+        renderBar(bubbleSort(unsortedArr, currI, currJ));
     });
 })
 
+//  -----------------SPEED-----------------
 document.addEventListener("DOMContentLoaded", function(){
     const speed_btn = document.getElementById("speedBtn");
     speed_btn.addEventListener("click", function(){
@@ -145,16 +179,18 @@ document.addEventListener("DOMContentLoaded", function(){
     })
 })
 
+//  -----------------BAR COUNT-----------------
 document.addEventListener("DOMContentLoaded", function(){
     const bars_btn = document.getElementById("barsBtn");
     bars_btn.addEventListener("click", function(){
-        barCount();
+        changeNumBars();
     })
 })
 
+//  -----------------RESUME-----------------
 document.addEventListener("DOMContentLoaded", function(){
     const resume_btn = document.getElementById("resumeBtn");
     resume_btn.addEventListener("click", function(){
-        resume();
+        resumeRunning();
     })
 })
